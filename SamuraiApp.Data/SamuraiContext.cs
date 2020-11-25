acdd3dbc4ce0ff9e8fe07daf46c1c02c0c4bc81b
humberto.mori@proximitycr.com <humberto.mori@proximitycr.com>
2020-11-25 18:35:56 -0500

    how to use entityframework in asp.net core application

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -6,6 +6,11 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext: DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options)
+            :base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
@@ -14,22 +19,6 @@ namespace SamuraiApp.Data
 
         public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
 
-        public static readonly ILoggerFactory ConsoleLoggerFactory
-            = LoggerFactory.Create(builder =>
-            {
-                builder
-               .AddFilter((category, level) =>
-                category == DbLoggerCategory.Database.Command.Name
-                && level == LogLevel.Information
-               ).AddConsole();
-            });
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                .UseLoggerFactory(ConsoleLoggerFactory).EnableSensitiveDataLogging()
-                .UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
